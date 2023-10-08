ab69bdb87c00e6200a97aac99170fc93230574b5
surgood86 <x-fisher86@yandex.ru>
2023-10-08 19:29:43 +0500

    update models.py

---
+++ content/models.py
@@ -0,0 +1,32 @@
+import os
+from django.db import models
+from users.models import User
+
+
+class Content(models.Model):
+    TYPES = (
+        ('news', 'News'),
+        ('article', 'Article'),
+    )
+
+    type = models.CharField(max_length=10, choices=TYPES, default='article')
+    title = models.TextField(null=False)
+    content = models.TextField(null=False)
+    image = models.ImageField(null=True, on_delete=models.SET_NULL)
+    video_url = models.URLField(null=True, blank=True)
+    author = models.ForeignKey(User, null=False, on_delete=models.SET_NULL)
+    datetime = models.DateTimeField(auto_now_add=True)
+    views_count = models.IntegerField(default=0)
+    likes_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return os.path.basename(self.image.name) if self.image else self.title
+
+
+class Comment(models.Model):
+    content = models.TextField(null=False)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='comments')
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username}: {self.content[:20]}"
